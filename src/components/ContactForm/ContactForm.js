a3e85b91a765b55ce467407009f79421a23a37e9
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-08-08 00:11:21 +0200

    2

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -5,7 +5,7 @@ export const ContactForm = ({ handler }) => {
       <input
         type="text"
         name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        pattern="^[A-Za-z.'\- ]+$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
       />
@@ -13,7 +13,7 @@ export const ContactForm = ({ handler }) => {
       <input
         type="tel"
         name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        pattern="^\+?\d{1,4}?\s?\(?\d{1,4}?\)?\s?\d{1,4}\s?\d{1,4}\s?\d{1,9}$"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
@@ -21,3 +21,7 @@ export const ContactForm = ({ handler }) => {
     </form>
   );
 };
+// ^[A-Za-z.'\- ]+$
+
+// ^\+?\d{1,4}?\s?\(?\d{1,4}?\)?\s?\d{1,4}\s?\d{1,4}\s?\d{1,9}$
+// \+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}
